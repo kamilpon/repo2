4e01e7f7147108d1f25fa60650951faaef723d13
kamilpon <139380569+kamilpon@users.noreply.github.com>
2023-08-16 16:14:27 +0300

    Delete app_lesson_4 directory

--- app_lesson_4/app_lesson_4/urls.py
+++
@@ -1,5 +0,0 @@
-from django.urls import path
-from . import views
-urlpatterns = [
-    path('lesson_4', views.lesson_4, name='lesson_4'),
-]
--- app_lesson_4/app_lesson_4/views.py
+++
@@ -1,3 +0,0 @@
-from django.http import HttpResponse
-def lesson_4(request):
-    return HttpResponse("Домашка по 4 занятию")
